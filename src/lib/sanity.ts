4717083c6fd9f039881d4192fd0a33452cf8f98a
Amal221200 <amalmurikkoli2000@gmail.com>
2024-06-23 22:36:55 +0530

    sanity

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.projectId;
-const dataset = process.env.dataset;
-const apiVersion = process.env.apiVersion;
+const projectId = process.env.SANITY_PROJECT_ID;
+const dataset = process.env.SANITY_DATASET;
+const apiVersion = process.env.SANITY_API_VERSION;
 const token = process.env.SANITY_API_TOKEN;
 
 export const sanity = createClient({ projectId, dataset, apiVersion, useCdn: false, token });
